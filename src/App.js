118075ff6d2bbdbc3d6402072cdc90f07f20b7ca
danielcagostinho <danielcagostinho@gmail.com>
2022-03-14 00:18:37 -0400

    Fixed Nav Drop Shadow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { isMobile } from "react-device-detect";
 
 import NavBar from "./components/NavBar/NavBar";
 import MobileNavBar from "./components/MobileNavBar/MobileNavBar";
@@ -152,7 +151,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {isMobile ? (
+        {window.innerWidth < 800 ? (
           <MobileNavBar />
         ) : (
           <NavBar showName={this.state.isNameDisplayed} />
